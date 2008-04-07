d700a255e6d61715af5731323319ad9ce1172c30
mbosteels.dns <mbosteels.dns@e336f92d-493e-0410-8f00-e9b859c1f311>
2008-04-07 21:36:59 +0000

    code cleanup: * remove usage of deprecated methods and interfaces * remove unused/commented code * remove usage of System.out.println

--- trunk/plugins/idea/JumpToCode/src/com/googlecode/firewood/plugins/intellij/JumpToCode/logic/JumpToCodeApplicationComponent.java
+++ trunk/plugins/idea/JumpToCode/src/com/googlecode/firewood/plugins/intellij/JumpToCode/logic/JumpToCodeApplicationComponent.java
@@ -1,27 +1,30 @@
 package com.googlecode.firewood.plugins.intellij.JumpToCode.logic;
 
 import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 
-import com.intellij.openapi.util.DefaultJDOMExternalizer;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.openapi.util.InvalidDataException;
-import com.intellij.openapi.util.JDOMExternalizable;
-import com.intellij.openapi.util.WriteExternalException;
+import com.intellij.util.xmlb.XmlSerializerUtil;
 import com.googlecode.firewood.plugins.intellij.JumpToCode.server.HttpServer;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.apache.log4j.Logger;
-import org.jdom.Element;
 
 import javax.swing.Icon;
 import javax.swing.JComponent;
 
+@State(
+    name =  "JumpToCodeApplicationComponent",
+    storages = {@Storage(id = "JumpToCode", file = "$OPTIONS$/JumpToCode.xml")}
+)
 public class JumpToCodeApplicationComponent implements ApplicationComponent, Configurable,
-  JDOMExternalizable {
+  PersistentStateComponent<Config> {
 
   static final Logger logger = (Logger) Logger.getInstance(JumpToCodeApplicationComponent.class);
 
@@ -76,19 +79,8 @@ public class JumpToCodeApplicationComponent implements ApplicationComponent, Con
   }
 
   public void initComponent() {
-     System.out.println("JumpToCodeApplicationComponent.initComponent");
-    // if config is different from default, readExternal should have been called
+    logger.debug("JumpToCodeApplicationComponent.initComponent");
     HttpServer.getInstance().configure(config);
-
-//    if (config.firstRun) {
-//      // during the first run (immediately after installation of the plugin)
-//      // readExternal will not be called because there is no configuration
-//      // => we just start the server with the default config
-//      this.createComponent().setVisible(true);
-//      HttpServer.getInstance().configure(config);
-//    }
-    // else we wait until readExternal is called
-    //config.firstRun = false;
   }
 
   public void disposeComponent() {
@@ -99,13 +91,13 @@ public class JumpToCodeApplicationComponent implements ApplicationComponent, Con
     return "JumpToCodeApplicationComponent";
   }
 
-  public void readExternal(Element element) throws InvalidDataException {
-    System.out.println("JumpToCodeApplicationComponent.readExternal " + element);
-    DefaultJDOMExternalizer.readExternal(config, element);
+  public Config getState() {
+    return config;
   }
 
-  public void writeExternal(Element element) throws WriteExternalException {
-    System.out.println("JumpToCodeApplicationComponent.writeExternal " + element);
-    DefaultJDOMExternalizer.writeExternal(config, element);
+  public void loadState(Config state) {
+    XmlSerializerUtil.copyBean(state, config);
+    logger.debug("loadState: config.enabled= " + state.isEnabled());
   }
+
 }
